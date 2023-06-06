218d5590afe5bbd093598c147e392ab9f7f5ca3d
David Ramia <david.ramia@processcontrol.es>
2023-06-06 21:08:39 +0200

    ...

--- account_banking_sepa_direct_debit/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import res_company
-from . import account_banking_mandate
-from . import account_payment_mode
-from . import account_payment_method
-from . import account_payment_order
-from . import account_payment_line
