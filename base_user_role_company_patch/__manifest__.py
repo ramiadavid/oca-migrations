b850acee4f64d36c63e725e533646dc1fa0bbf3c
David Ramia <david.ramia@processcontrol.es>
2025-01-19 16:41:39 +0100

    ...

---
+++ base_user_role_company_patch/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "User roles by company | Patch",
+    "version": "17.0.1.1.1",
+    "category": "Tools",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-backend",
+    "depends": ["base_user_role"],
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+    "maintainer": "dreispt",
+    "development_status": "Beta",
+}
