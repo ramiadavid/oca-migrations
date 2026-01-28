baf74c34c88930b0e46b57da312381444f835998
David Ramia <david.ramia@processcontrol.es>
2026-01-28 14:09:12 +0100

    no message

--- report_qweb_parameter/tests/test_report_qweb_parameter.py
+++ report_qweb_parameter/tests/test_report_qweb_parameter.py
@@ -5,7 +5,6 @@ import xml.etree.ElementTree as ET
 
 from odoo.tests import common
 
-from odoo.addons.base.models.ir_qweb import QWebException
 
 
 class TestReportQWebParameter(common.TransactionCase):
